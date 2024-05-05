948e49a1ff4ae4e412b0d84da1b673069bcd22f6
Bekbergenova Alma <alma02071988@gmail.com>
2024-05-05 20:25:15 +0500

    conflict

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 browser.open('https://school.qa.guru')
 browser.element('[name="email"]').type('a.b@tele2.kz').press_enter()
-browser.element('[name="password"]').type('+++++').press_enter().press_enter()
+browser.element('[name="пароль"]').type('+++++').press_enter().press_enter()
 browser.element('[class="page-header"]').should(have.text('Список тренингов'))
